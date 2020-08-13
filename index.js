1de5e36fb8a708e6e5dc1a0b9a25eec8c0926f51
Christopher Shelley <dont@forgetmy.email>
2020-08-13 15:17:36 +0200

    add CORS middleware, put port in .env, change port number

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const postsRouter = require("./routes/posts")
+const cors = require("cors")
 require("dotenv").config()
 require('./database/client')
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 app.use('/api/posts', postsRouter);
-app.listen(3000, () => console.log("running on 3000"))
+app.listen(process.env.PORT, () => console.log("running on " + process.env.PORT))
